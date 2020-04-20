b9bd42abb8bd8ec5b1248441de5460839c3f1abf
Timo Tuomi <ttuomi@gmail.com>
2020-04-20 17:35:15 +0300

    3.1

--- index.js
+++ index.js
@@ -2,34 +2,49 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+let persons = [
+    { 
+      name: "Arto Hellas", 
+      number: "040-123456",
+      id: 1
+    },
+    { 
+      name: "Ada Lovelace", 
+      number: "39-44-5323523",
+      id: 2
+    },
+    { 
+      name: "Dan Abramov", 
+      number: "12-43-234345",
+      id: 3
+    },
+    { 
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122",
+      id: 4
+    }
+  ]
+
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  response.json(person)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  res.send(`Phonebook has info for ${persons.length} persons<p>${new Date()}</p>`)
 })
 
 const PORT = 3001
