ba48d3f3f6cf1e1e1bad735272e21fc18c8a5ef2
Timo Tuomi <ttuomi@gmail.com>
2020-04-26 09:35:15 +0300

    uibuild

---
+++ build/precache-manifest.fb3949ec123181ad95ee7b5b7039960a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7df56e505786ef1690b0bf10cc1c37d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50b2e459d25667e2841c",
+    "url": "/static/js/2.37c0a557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37c0a557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d025afb3ea793cc419",
+    "url": "/static/js/main.5bd80af5.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-//app.use(express.static('build'))
+app.use(express.static('build'))
 
 // morgan.token('bodystr', (req, res) => {
 //   return JSON.stringify(req.body)
